f45238d8071f5ae741a638ca2a00e4299d2221fd
npaik <nakhyunpaik@gmail.com>
2023-10-13 18:24:10 -0700

    header

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,7 +5,7 @@ export default function Container({ children }: { children: React.ReactNode }) {
     <div className="container">
       <header className="header">
         <Link className="header__title" href="/">
-          Next.js + Sanity
+          GA-ON Reastaurant Blog
         </Link>
       </header>
       <main>{children}</main>
